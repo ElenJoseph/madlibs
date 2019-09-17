ec48b3cf62ea294a86a0dc826a1826814067f89d
Elen Joseph <youremailaddress@algonquinlive.comjose0205@algonquinlive.com>
2019-09-17 16:28:41 -0400

    first commit madlibs

---
+++ script.js
@@ -0,0 +1,102 @@
+const littleRed = {
+    color:'green',
+    pluralNoun:['countries','bikes','planes','ships'],
+    adjective:'awsome',
+    exclamation:'!',
+    sillyWord:'fuzzy',
+    verb: ['made','create','drive']
+}
+console.log(`One day, Little ${littleRed.color} Riding Hood was going through the forest carrying a 
+basket of ${littleRed.pluralNoun [1]} for her grandmother. Suddenly, she met a big 
+${littleRed.adjective} wolf.
+
+"${littleRed.exclamation}" said the wolf. "Where are you going little ${littleRed.sillyWord}?"
+
+"I'm going to my grandmother's house," she said. Then the wolf 
+${littleRed.verb [1]} away.
+
+When Miss Riding Hood got to her grandmother's house, the wolf was in bed 
+dressed like her grandmother.
+
+"My, Grandmother," she said. "What big ${littleRed.pluralNoun [2]} you have."
+
+"The better to ${littleRed.verb [2]} you with," said the wolf.
+
+"And, Grandmother," she said, "what big ${littleRed.pluralNoun [3]}" you have."
+
+The wolf said, "The better to ${littleRed.verb [3]} you with."
+
+And then she said, "What big ${littleRed.pluralNoun [4]}" you have, Grandmother." But 
+the wolf said nothing. He had just died of indigestion from eating Grandmother`)
+
+
+littleRed.color = 'pink'
+littleRed.pluralNoun[2]= 'car'
+littleRed.exclamation = '?'
+littleRed.verb[1]='go'
+
+console.log(`One day, Little ${littleRed.color} Riding Hood was going through the forest carrying a 
+basket of ${littleRed.pluralNoun [1]} for her grandmother. Suddenly, she met a big 
+${littleRed.adjective} wolf.
+
+"${littleRed.exclamation}" said the wolf. "Where are you going little ${littleRed.sillyWord}?"
+
+"I'm going to my grandmother's house," she said. Then the wolf 
+${littleRed.verb [1]} away.
+
+When Miss Riding Hood got to her grandmother's house, the wolf was in bed 
+dressed like her grandmother.
+
+"My, Grandmother," she said. "What big ${littleRed.pluralNoun [2]} you have."
+
+"The better to ${littleRed.verb [2]} you with," said the wolf.
+
+"And, Grandmother," she said, "what big ${littleRed.pluralNoun [3]}" you have."
+
+The wolf said, "The better to ${littleRed.verb [3]} you with."
+
+And then she said, "What big ${littleRed.pluralNoun [4]}" you have, Grandmother." But 
+the wolf said nothing. He had just died of indigestion from eating Grandmother`)
+
+const snowWhite = {
+    pluralNoun:['apples','bananas',],
+    number:7,
+    adjective:['beautifull','tall','lazy'],
+    noun:['park','house','bad','food','spoon'],
+    color:'wite',
+    partOfTheBody:'feet',
+    adverb: 'badly'
+}
+
+console.log(`One of the most popular fairy ${snowWhite.pluralNoun [1]} of all time is Snow White and the 
+${snowWhite.number} ${snowWhite.pluralNoun [2]}.
+
+Snow White is a princess whose ${snowWhite.adjective [1]} beauty threatens her stepmother, 
+the queen. Snow White is forced to flee from the ${snowWhite.noun [1]} in which she lives 
+and hide in a nearby ${snowWhite.noun [2]}.
+
+Once there, she is discovered by ${snowWhite.adjective [2]} animals who guide her to the 
+${snowWhite.adjective [3]} cottage of the seven dwarfs. The dwarfs take care of her until 
+the prince, who has traveled the four corners of the [noun 3] in search of 
+Snow ${snowWhite.color}, arrives and gives her a magical ${snowWhite.noun [4]} on her 
+${snowWhite.partOfTheBody}, which miraculously bring her back to life after she eats 
+a poisonous ${snowWhite.noun [5]}. Snow White and the prince live ${snowWhite.adverb} ever after.`)
+
+snowWhite.number = 1
+snowWhite.adjective[0]= 'inteligent'
+lsnowWhite.noun[3] = apple
+snowWhite.color = 'red'
+
+console.log(`One of the most popular fairy ${snowWhite.pluralNoun [1]} of all time is Snow White and the 
+${snowWhite.number} ${snowWhite.pluralNoun [2]}.
+
+Snow White is a princess whose ${snowWhite.adjective [1]} beauty threatens her stepmother, 
+the queen. Snow White is forced to flee from the ${snowWhite.noun [1]} in which she lives 
+and hide in a nearby ${snowWhite.noun [2]}.
+
+Once there, she is discovered by ${snowWhite.adjective [2]} animals who guide her to the 
+${snowWhite.adjective [3]} cottage of the seven dwarfs. The dwarfs take care of her until 
+the prince, who has traveled the four corners of the [noun 3] in search of 
+Snow ${snowWhite.color}, arrives and gives her a magical ${snowWhite.noun [4]} on her 
+${snowWhite.partOfTheBody}, which miraculously bring her back to life after she eats 
+a poisonous ${snowWhite.noun [5]}. Snow White and the prince live ${snowWhite.adverb} ever after.`)
